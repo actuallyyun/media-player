3a52f25b9c3b23b8b66b3a18f09f0782f60d04c6
Yun Ji <this.jiyun@gmail.com>
2024-04-04 11:05:34 +0300

    modify program.cs

--- MediaPlayer.Infrastructure/Program.cs
+++ MediaPlayer.Infrastructure/Program.cs
@@ -7,52 +7,61 @@ using MediaPlayer.Service.src.Service;
 
 internal class Program
 {
+
+    public static Admin CreateAdmin(){
+        var admin=new Admin("admin","admin@mail.com","Admin");
+        return admin;
+    }
     private static void Main(string[] args)
     {
-        // create user and media via admin service
+        // Test user service
         var db = new Database();
-        var mediaRepo = new MediaRepository(db);
         var userRepo = new UserRepository(db);
+        var admin=CreateAdmin();
+
+        // create user service
+        var userService=new UserService(userRepo,admin);
+        var admin2 = new UserCreateDto("admin2", "admin@gmail.com", "Admin 2", true);
+        var user1 = new UserCreateDto("user1", "user1@gmail.com", "User 1", false);
+
+        var user=userService.AddUser(user1);
+        userService.AddUser(admin2);
+
+        // Test media service
+        var mediaRepo = new MediaRepository(db);
+        var mediaService=new MediaService(mediaRepo,admin);
+
+        var audio1 = new MediaCreateDto(MediaType.Audio, "audio", "yun", 1995);
+        var video1 = new MediaCreateDto(MediaType.Video, "video", "dane", 2015);
+
+        var audio=mediaService.AddMedia(audio1);
+        var video=mediaService.AddMedia(video1);
+
+        // Test playlist service
+
         var playListRepo = new PlaylistRepository(db);
-        var adminService = new AdminService(mediaRepo, userRepo);
+        var playListService=new PlayListService(playListRepo,user);
 
-        var mediaCreate1 = new MediaCreateDto(MediaType.Audio, "audio", "yun", 1995);
-        var mediaCreate2 = new MediaCreateDto(MediaType.Video, "video", "dane", 2015);
 
-        var user1 = new UserCreateDto("yunyun", "yun@gmail.com", "Yun Ji", true);
-        var user2 = new UserCreateDto("yunyun", "yun@gmail.com", "Yun Ji", false);
+        var playList1 = new PlayListCreateDTO(user.Id, "Yun playlist 1", false);
+        var playlist2 = new PlayListCreateDTO(user.Id, "My Playlist 1");
 
-        var audio1 = adminService.AddMedia(mediaCreate1);
-        var video1 = adminService.AddMedia(mediaCreate2);
+        var playlsit=playListService.CreateNewPlaylist(playList1);
+        playListService.CreateNewPlaylist(playlist2);
 
-        var user1Created = adminService.AddUser(user1);
-        var user2Created = adminService.AddUser(user2);
 
-        // create a new playlist via user service
-
-        var user1Service = new UserService(playListRepo, user1Created);
-        var user2Service = new UserService(playListRepo, user2Created);
-
-        var playList1 = new PlayListCreateDTO(user1Created.Id, "Yun playlist 1", false);
-        var playlist2 = new PlayListCreateDTO(user2Created.Id, "My Playlist 1");
-
-        var playListCreated1 = user1Service.CreateNewPlaylist(playList1);
-        var playListCreated2 = user1Service.CreateNewPlaylist(playlist2);
-        // Add media to playlist
-        user1Service.AddMediaToPlayList(playListCreated1.Id, audio1);
-        
-        // add playlist
-        user2Service.AddPlaylistById(playListCreated1.Id);
+        // PlayList control service
+        var playListController=new PlayListControlService(playlsit,user);
 
         // play,stop,pause playlist
-        user1Service.PlayPlaylist(playListCreated1);
-        user1Service.StopPlaylist(playListCreated1);
-        user1Service.PausePlaylist(playListCreated1);
+        playListController.PlayPlaylist();
+        playListController.StopPlaylist();
+        playListController.PausePlaylist();
 
         // change sound effect or brightness
-        user1Service.ChangeSoundEffect(audio1, "base");
-        user1Service.ChangeSoundEffect(video1, "base");
-        user1Service.ChangeBrightness(audio1, "bright");
-        user1Service.ChangeBrightness(video1, "bright");
+        playListController.ChangeSoundEffect(audio, "base");
+        playListController.ChangeSoundEffect(video, "base");
+        playListController.ChangeBrightness(audio, "bright");
+        playListController.ChangeBrightness(video, "bright");
     }
 }
