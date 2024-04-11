279a91b7a25fa10e0568e7da5ea4786cff72ddbf
Yun Ji <this.jiyun@gmail.com>
2024-04-11 11:07:56 +0300

    test: add UserFactory tests

---
+++ MediaPlayer.Tests/src/Service/Utils/UserFactoryTests.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediaPlayer.Core.src.Enums;
+using MediaPlayer.Service.src.Utils;
+using Xunit;
+
+namespace MediaPlayer.Tests.src.Service.Utils
+{
+    public class UserFactoryTests
+    {
+        [Fact]
+        public void Create_WithUserType_ShouldReturnUserType()
+        {
+            var userFactory = new UserFactory();
+            var result = userFactory.Create(TestUtils.UserCreate);
+            Assert.Equal(UserType.User, result.Type);
+        }
+
+        [Fact]
+        public void Create_WithAdminType_ShouldReturnAdminType()
+        {
+            var userFactory = new UserFactory();
+            var result = userFactory.Create(TestUtils.AdminCreate);
+            Assert.Equal(UserType.Admin, result.Type);
+        }
+    }
+}
