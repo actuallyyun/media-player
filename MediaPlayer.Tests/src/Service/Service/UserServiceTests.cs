229952ee83a5a551ff3252b3fdaa9e41f2f453dc
Yun Ji <this.jiyun@gmail.com>
2024-04-11 11:37:12 +0300

    test:add UserServiceTests

---
+++ MediaPlayer.Tests/src/Service/Service/UserServiceTests.cs
@@ -0,0 +1,109 @@
+using MediaPlayer.Core.src.Entity;
+using MediaPlayer.Core.src.Enums;
+using MediaPlayer.Core.src.RepositoryAbstraction;
+using MediaPlayer.Service.Service;
+using MediaPlayer.Service.src.DTO;
+using Moq;
+
+namespace MediaPlayer.Tests.src.Service.Service
+{
+    public class UserServiceTests
+    {
+        private Mock<IUserRepository> _mockUserRepo = new Mock<IUserRepository>();
+
+        private Mock<Admin> _mockAdmin = new Mock<Admin>("admin", "admin", "Admin");
+
+        [Fact]
+        public void AddUser_WithValidUserCreateData_ShouldAddAndReturnUserType()
+        {
+            var testUserName = "test";
+            User? userFound = null;
+            _mockUserRepo.Setup(x => x.GetUserByName(testUserName)).Returns(userFound);
+            var userService = new UserService(_mockUserRepo.Object, _mockAdmin.Object);
+            var result = userService.AddUser(TestUtils.UserCreate);
+            Assert.Equal(UserType.User, result.Type);
+            Assert.Equal(result.Username, TestUtils.UserCreate.Username);
+            _mockUserRepo.Verify(x => x.Add(It.IsAny<User>()), Times.Once());
+        }
+
+        [Fact]
+        public void AddUser_WithValidAdminCreateData_ShouldAddAndReturnAdminType()
+        {
+            var testUserName = "test";
+            User? userFound = null;
+            _mockUserRepo.Setup(x => x.GetUserByName(testUserName)).Returns(userFound);
+            var userService = new UserService(_mockUserRepo.Object, _mockAdmin.Object);
+            var result = userService.AddUser(TestUtils.AdminCreate);
+            Assert.Equal(UserType.Admin, result.Type);
+            Assert.Equal(result.Username, TestUtils.AdminCreate.Username);
+            _mockUserRepo.Verify(x => x.Add(It.IsAny<User>()), Times.Once());
+        }
+
+        [Fact]
+        public void AddUser_WithExistingUsername_ShouldNotAddAndReturnNull()
+        {
+            _mockUserRepo
+                .Setup(x => x.GetUserByName(TestUtils.UserCreate.Username))
+                .Returns(TestUtils.User1);
+            var userService = new UserService(_mockUserRepo.Object, _mockAdmin.Object);
+            var result = userService.AddUser(TestUtils.UserCreate);
+            Assert.Null(result);
+            _mockUserRepo.Verify(x => x.Add(It.IsAny<User>()), Times.Never);
+        }
+
+        [Fact]
+        public void DeleteUserById_WithFoundUser_ShouldDeleteAndReturnTrue()
+        {
+            var id = Guid.NewGuid();
+            _mockUserRepo.Setup(x => x.GetUserById(id)).Returns(TestUtils.User1);
+            _mockUserRepo.Setup(x => x.Remove(TestUtils.User1));
+            var userService = new UserService(_mockUserRepo.Object, _mockAdmin.Object);
+            var result = userService.DeleteUserById(id);
+            Assert.True(result);
+            _mockUserRepo.Verify(); // Assert both mocked repo methods should be called with provided parameters
+        }
+
+        [Fact]
+        public void DeleteUserById_WithUnFoundUser_ShouldNotDeleteAndReturnFalse()
+        {
+            var id = Guid.NewGuid();
+            User? user = null;
+            _mockUserRepo.Setup(x => x.GetUserById(id)).Returns(user);
+            var userService = new UserService(_mockUserRepo.Object, _mockAdmin.Object);
+            var result = userService.DeleteUserById(id);
+            Assert.False(result);
+            _mockUserRepo.Verify(x => x.Remove(It.IsAny<User>()), Times.Never); // Assert Remove not be called
+        }
+
+        [Fact]
+        public void UpdateUser_WithUserFound_ShouldUpdateAndReturnTrue()
+        {
+            var id = Guid.NewGuid();
+            _mockUserRepo.Setup(x => x.GetUserById(id)).Returns(TestUtils.User1);
+            var userService = new UserService(_mockUserRepo.Object, _mockAdmin.Object);
+            var result = userService.UpdateUser(id, TestUtils.validUserUpdate);
+            Assert.True(result);
+            _mockUserRepo.Verify();
+        }
+
+        [Fact]
+        public void UpdateUser_WithUserNotFound_ShouldNotUpdateAndReturnFalse()
+        {
+            var id = Guid.NewGuid();
+            User? user = null;
+            _mockUserRepo.Setup(x => x.GetUserById(id)).Returns(user);
+            var userService = new UserService(_mockUserRepo.Object, _mockAdmin.Object);
+            var result = userService.UpdateUser(id, TestUtils.validUserUpdate);
+            Assert.False(result);
+            _mockUserRepo.Verify(
+                x =>
+                    x.Update(
+                        TestUtils.User1,
+                        TestUtils.validUserUpdate.Email,
+                        TestUtils.validUserUpdate.FullName
+                    ),
+                Times.Never
+            );
+        }
+    }
+}
--- MediaPlayer.Tests/src/Service/TestUtils.cs
+++ MediaPlayer.Tests/src/Service/TestUtils.cs
@@ -1,13 +1,12 @@
-using MediaPlayer.Service.src.DTO;
-using MediaPlayer.Core.src.Enums;
-using Microsoft.VisualBasic;
 using MediaPlayer.Core.src.Entity;
+using MediaPlayer.Core.src.Enums;
+using MediaPlayer.Service.src.DTO;
+using Microsoft.VisualBasic;
 
 namespace MediaPlayer.Tests.src.Service
 {
     public static class TestUtils
     {
-        
         public static MediaCreateDto InvalidYear1 = new MediaCreateDto(
             MediaType.Audio,
             "test audio",
@@ -22,11 +21,25 @@ namespace MediaPlayer.Tests.src.Service
             DateAndTime.Now.AddYears(10).Year
         );
 
-        public static IEnumerable<object[]> InvalidMediaData => [
-                new object[] { InvalidYear1 },
-                new object[] { InvalidYear2 }
-            ];
+        public static IEnumerable<object[]> InvalidMediaData =>
+            [new object[] { InvalidYear1 }, new object[] { InvalidYear2 }];
 
-        public static Media Media1=>new Audio("audio1","audio",2000);
+        public static Media Media1 => new Audio("audio1", "audio", 2000);
+        public static User User1 => new User("user", "user@mail.com", "User");
+
+        public static UserCreateDto UserCreate = new UserCreateDto(
+            "user1",
+            "user",
+            "User",
+            UserType.User
+        );
+        public static UserCreateDto AdminCreate = new UserCreateDto(
+            "user2",
+            "user",
+            "User",
+            UserType.Admin
+        );
+        public static UserUpdateDto validUserUpdate = new UserUpdateDto("user1", null);
+        public static UserUpdateDto invalidUserUpdate = new UserUpdateDto("usernoneexisting", null);
     }
 }
