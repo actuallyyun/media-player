9cdafb753e09a2bd9738e5c8fb0a51a0eff8daab
Yun Ji <this.jiyun@gmail.com>
2024-04-11 11:08:14 +0300

    fix:bug in Admin

--- MediaPlayer.Core/src/Entity/Admin.cs
+++ MediaPlayer.Core/src/Entity/Admin.cs
@@ -6,7 +6,7 @@ namespace MediaPlayer.Core.src.Entity
     public class Admin : User
     {
 
-        public override UserType Type => UserType.User;
+        public override UserType Type => UserType.Admin;
         public Admin(string username, string email, string fullName)
             : base(username, email, fullName) { 
                 
